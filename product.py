4c5f533a78b9218926d8c50afbde4f46698aa362
logapriyalaksh <47312306+logapriyalaksh@users.noreply.github.com>
2019-05-06 11:54:57 +0530

    Create product.py

---
+++ product.py
@@ -0,0 +1,17 @@
+def find_product(num1,num2,num3):
+    product=0
+    
+    if (num1==7):
+        product=num2*num3
+    elif(num2==7):
+        product=num3
+    elif(num3==7):
+        product=-1
+    else:
+        product=num1*num2*num3
+        
+    return product
+
+
+product=find_product(7,6,2)
+print(product)
